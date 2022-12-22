c6c44a36e1c0b3302286d4bb67b7b89910a51f06
sankarshandev98 <gsankar164@gmail.com>
2022-12-22 22:43:54 +0530

    update

--- lms/Components/Navbar/Chat.jsx
+++ lms/Components/Navbar/Chat.jsx
@@ -1,6 +1,8 @@
 import {
+  Box,
   Button,
   Container,
+  Flex,
   Image,
   Input,
   Popover,
@@ -11,6 +13,7 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 import React from "react";
 import { BsChatRightDots } from "react-icons/bs";
@@ -38,14 +41,27 @@ export default function Chat() {
           <PopoverHeader background-color={"blue.100"}>
             Message Us!
           </PopoverHeader>
-          <PopoverBody>
+          <PopoverBody gap={"50px"}>
             <Image
               src="https://www.manageengine.com/products/service-desk/help-desk-software/images/what-is-help-desk-software.png"
               alt="help"
             />
-            <Text textAlign={"center"} fontSize="xl" pb={"3"} pt="3">LMS Support Panel</Text>
-            <Input></Input>
-            <Button>Enter</Button>
+            <Text textAlign={"center"} fontSize="xl" pb={"3"} pt="3">
+              LMS Support Panel
+            </Text>
+            <Text
+              border={"2px"}
+              borderRadius="15"
+              textAlign={"center"}
+              mb="20px"
+            >
+              Welcome! We are excited to offer you messaging, allowing you to
+              respond to us in your own time. Click here to get started.
+            </Text>
+            <Flex alignItems={"end"} gap="2">
+              <Textarea borderRadius={"15"}></Textarea>
+              <Button borderRadius={"15"}>Enter</Button>
+            </Flex>
           </PopoverBody>
         </PopoverContent>
       </Popover>
