a0b2bcca48599013007839b1959b4cbd3ac76af6
sankarshandev98 <gsankar164@gmail.com>
2022-12-23 00:30:24 +0530

    update

---
+++ lms/pages/socketio.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import io from "socket.io-client";
+
+export default () => {
+  useEffect(() => {
+    fetch("/api/socketio").finally(() => {
+      const socket = io();
+
+      socket.on("connect", () => {
+        console.log("connect");
+        socket.emit("hello");
+      });
+
+      socket.on("hello", (data) => {
+        console.log("hello", data);
+      });
+
+      socket.on("a user connected", () => {
+        console.log("a user connected");
+      });
+
+      socket.on("disconnect", () => {
+        console.log("disconnect");
+      });
+    });
+  }, []); // Added [] as useEffect filter so it will be executed only once, when component is mounted
+
+  return <h1>Socket.io</h1>;
+};
