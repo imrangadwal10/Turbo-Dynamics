5e55bcf1cbadea54b4b3cd67fa89fb63f8bb5c70
sankarshandev98 <gsankar164@gmail.com>
2022-12-23 13:59:41 +0530

    update

--- lms/pages/contact-us.jsx
+++ lms/pages/contact-us.jsx
@@ -16,62 +16,63 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+// import io from "socket.io-client";
 import { BsChatRightDots } from "react-icons/bs";
-const Message = {
-  author: "",
-  message: "",
-};
+// const Message = {
+//   author: "",
+//   message: "",
+// };
 export default function ContactUs() {
-  const [username, setUsername] = useState("");
-  const [chosenUsername, setChosenUsername] = useState("");
-  const [landing, setLanding] = useState(true);
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+  // const [username, setUsername] = useState("");
+  // const [chosenUsername, setChosenUsername] = useState("");
+  // const [landing, setLanding] = useState(true);
+  // const [message, setMessage] = useState("");
+  // const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    socketInitializer();
-  }, []);
+  // useEffect(() => {
+  //   socketInitializer();
+  // }, []);
 
-  const socketInitializer = async () => {
-    // We just call it because we don't need anything else out of it
-    await fetch("/api/socketio");
+  // const socketInitializer = async () => {
+  //   // We just call it because we don't need anything else out of it
+  //   await fetch("/api/socketio");
 
-    let socket = io();
+  //   let socket = io();
 
-    socket.on("newIncomingMessage", (msg) => {
-      setMessages((currentMsg) => [
-        ...currentMsg,
-        { author: msg.author, message: msg.message },
-      ]);
-      console.log(messages);
-    });
-  };
+  //   socket.on("newIncomingMessage", (msg) => {
+  //     setMessages((currentMsg) => [
+  //       ...currentMsg,
+  //       { author: msg.author, message: msg.message },
+  //     ]);
+  //     console.log(messages);
+  //   });
+  // };
 
-  const sendMessage = async () => {
-    let socket = io();
-    socket.emit("createdMessage", { author: chosenUsername, message });
-    setMessages((currentMsg) => [
-      ...currentMsg,
-      { author: chosenUsername, message },
-    ]);
-    setMessage("");
-  };
+  // const sendMessage = async () => {
+  //   let socket = io();
+  //   socket.emit("createdMessage", { author: chosenUsername, message });
+  //   setMessages((currentMsg) => [
+  //     ...currentMsg,
+  //     { author: chosenUsername, message },
+  //   ]);
+  //   setMessage("");
+  // };
 
-  const handleKeypress = (e) => {
-    //it triggers by pressing the enter key
-    if (e.keyCode === 13) {
-      if (message) {
-        sendMessage();
-      }
-    }
-  };
+  // const handleKeypress = (e) => {
+  //   //it triggers by pressing the enter key
+  //   if (e.keyCode === 13) {
+  //     if (message) {
+  //       sendMessage();
+  //     }
+  //   }
+  // };
 
   return (
     <Container>
       <Popover>
         <PopoverTrigger>
           <Button
+            // onClick={() => setLanding(!landing)}
             colorScheme="teal"
             gap={"2"}
             borderRadius="25"
